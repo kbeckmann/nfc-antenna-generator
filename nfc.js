322957b822208fedab2ec1ca053ee5067ad53b06
Konrad Beckmann <konrad.beckmann@gmail.com>
2016-06-19 03:52:37 +0200

    Fixed all the things

--- nfc.js
+++ nfc.js
@@ -1,6 +1,5 @@
 // Units in mm
-function nfc(margin, scale, turns, ant_length, ant_width, cond_width, cond_spacing) {
-    var c = document.getElementById("myCanvas");
+function nfc(c, margin, scale, turns, ant_length, ant_width, cond_width, cond_spacing) {
     c.height = scale * ant_length + 2 * margin;
     c.width = scale * ant_width + 2 * margin;
     var ctx = c.getContext("2d");
@@ -9,12 +8,6 @@ function nfc(margin, scale, turns, ant_length, ant_width, cond_width, cond_spaci
     ctx.fillStyle = "#FFFFFF";
     ctx.fillRect(0,0,scale * ant_width + 2 * margin, scale * ant_length + 2 * margin);
     ctx.translate(margin, margin);
-
-    function download() {
-        var dt = c.toDataURL('image/png');
-        this.href = dt;
-    };
-    downloadLnk.addEventListener('click', download, false);
 
     var segments = turns * 4;
 
@@ -33,31 +26,31 @@ function nfc(margin, scale, turns, ant_length, ant_width, cond_width, cond_spaci
             //ctx.fillStyle = "#00FF00";
             x0 = ant_width - ((segment-1) / 4) * (cond_width + cond_spacing) - cond_width;
             y0 = Math.floor(segment / 4) * (cond_width + cond_spacing);
-            x1 = ant_width - ((segment-1) / 4) * (cond_width + cond_spacing);
+            x1 = x0 + cond_width + 1/scale;
             y1 = ant_length - Math.floor(segment / 4) * (cond_width + cond_spacing);
             break;
             case 2:
             //ctx.fillStyle = "#0000FF7f";
             x0 = Math.floor(segment / 4) * (cond_width + cond_spacing);
             y0 = ant_length - Math.floor(segment / 4) * (cond_width + cond_spacing) - cond_width;
-            x1 = ant_width - (segment / 4) * (cond_width + cond_spacing) + cond_width;
-            y1 = y0 + cond_width;
+            x1 = ant_width - Math.floor(segment / 4) * (cond_width + cond_spacing);
+            y1 = y0 + cond_width + 1/scale;
             break;
             case 3:
-            //ctx.fillStyle = "#FF00FF";
+            //ctx.fillStyle = "#FF00FF7F";
             x0 = Math.floor((segment-3) / 4) * (cond_width + cond_spacing);
             y0 = Math.floor(segment / 4) * (cond_width + cond_spacing) + cond_width + cond_spacing;
-            x1 = Math.floor((segment-3) / 4) * (cond_width + cond_spacing) + cond_width + 1/scale;
+            x1 = x0 + cond_width + 1/scale;
             y1 = ant_length - Math.floor(segment / 4) * (cond_width + cond_spacing);
             break;
         }
 
         console.log(segment, segment%4, x0, y0, x1, y1);
         ctx.fillRect(
-            Math.ceil(scale*x0),
-            Math.ceil(scale*y0),
-            Math.ceil(scale*(x1-x0)),
-            Math.ceil(scale*(y1-y0))
+            Math.floor(scale*x0),
+            Math.floor(scale*y0),
+            Math.floor(scale*(x1-x0)),
+            Math.floor(scale*(y1-y0))
         );
     }
 
@@ -75,21 +68,48 @@ function nfc(margin, scale, turns, ant_length, ant_width, cond_width, cond_spaci
             ctx.fillRect(scale*x0, scale*y0, w, w);
         }
     }
-
-
 }
 
+var canvas = document.createElement('canvas');
+canvas.style.border = "1px solid";
+document.body.appendChild(canvas);
+
+
+document.getElementById("downloadLnk").addEventListener("click", function(){
+    var dt = canvas.toDataURL('image/png');
+    this.href = dt;
+});
+
+canvas.addEventListener("click", function(){
+    var dt = canvas.toDataURL('image/png');
+    document.location = dt;
+});
+
+function generate() {
+    function value(id, def) {
+        return parseFloat(document.getElementById(id).value) || def;
+    }
+    nfc(canvas,
+        value("margin", 20),
+        value("dpi", 600)/25.4,
+        value("turns", 3),
+        value("ant_length", 10),
+        value("ant_width", 20),
+        value("cond_width", 0.6),
+        value("cond_spacing", 0.6));
+}
+document.getElementById("generate").addEventListener("click", generate);
+
+
+generate();
+
+// for testing stupid rounding errors
 if (0) {
     var dpi = 207;
     setInterval(function(){
         dpi += 1;
-        nfc(0, dpi / 25.4, 3, 20, 30, 1, 2)
+        nfc(canvas,0, dpi / 25.4, 3, 20, 30, 1, 2)
     }, 50);
 }
 
 
-var dpi = 1600;
-nfc(0, dpi/25.4, 8, 18, 30, 0.5, 0.6)
-
-
-//nfc(0, 20, 3, 20, 20, 1, 2)
